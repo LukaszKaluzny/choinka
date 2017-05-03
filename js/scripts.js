d7b407af6591dfba31cc55ec83ec7ea62bc8e76a
Lukasz Szczesniak <veiner87@gmail.com>
2017-05-03 18:45:16 +0200

    branch with master

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var i,j;
+function drawTree (i) {
+	for (i = 0; i <= 4; i++)
+	{
+		var star = ' ';
+    	for (j = 1; j <= i; j++)
+    	{
+    	var	starAfter = star.replace(' ', '*');
+
+    	// document.write("*");
+    	// console.log("*");
+    	}
+    	console.log (starAfter);
+	}
+}
