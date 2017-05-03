a2d71d2da762ebba07db3c6762bbe9a99cbdfb2e
Lukasz Szczesniak <veiner87@gmail.com>
2017-05-03 21:21:55 +0200

    repaired tree :-P

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-var i,j;
-function drawTree (i) {
-	for (i = 0; i <= 4; i++)
+
+function drawTree (pietra) {
+	for (var i = 1; i <= pietra; i++)
 	{
 		var star = ' ';
-    	for (j = 1; j <= i; j++)
-    	{
-    	var	starAfter = star.replace(' ', '*');
-
-    	// document.write("*");
-    	// console.log("*");
-    	}
-    	console.log (starAfter);
+    		for (var j = 1; j <= i; j++)
+    		{
+    			star=star+"*";
+    		}
+    	console.log(star);
+    	document.write(star);
+    	document.write('<br>');
 	}
 }
+pietra = prompt ('Podaj ilość pięter');
+drawTree(pietra);
