08862dc12a3ee9269ec9a51fc5c7627eed90bb00
Lukasz Szczesniak <veiner87@gmail.com>
2017-05-04 19:20:52 +0200

    branch with master

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 
 function drawTree (floor) {
-	for (var i = 1; i <= floor; i++) {
-		var star = ' ';
-    	for (var j = 1; j <= i; j++) {
-    		star += '*';
-    	}
-    	console.log(star);
-	}
+  for (var i = 1; i <= floor; i++) {
+    var star = ' ';
+    for (var j = 1; j <= i; j++) {
+      star += '*';
+    }
+    console.log(star);
+  }
 }
 floor = prompt ('Podaj ilość pięter');
 drawTree (floor);
